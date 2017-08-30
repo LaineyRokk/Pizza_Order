cfe2a7376317e7e1dfaadc142bdb3e4df13cc1f5
lainey <laineyNrokk@gmail.com>
2017-08-30 10:58:20 -0700

    make a few changes to scripts.js; add styles.css info

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,14 @@
 //Business Logic
 
-//Object(pizza) Constructor
 function Pizza(size) {
   this.piesize = size;
   this.pietoppings = [];
 };
 
-//Object(pizza) Prototype Method
 Pizza.prototype.price = function() {
   var price = 12;
 
-  if(this.piesize === "gargantuan") {
+  if(this.piesize === "ex-large") {
     price += 8;
   } else if (this.piesize === "large") {
     price += 4;
@@ -19,28 +17,30 @@ Pizza.prototype.price = function() {
   } else {
     price += 0;
   }
+
   this.pieToppings.forEach(function() {
     price += 1.5;
-  });
+  }
+
   this.price = price;
-}
+});
 
 //User Interface Logic
 $(document).ready(function() {
   $("form#order-form").submit(function(event) {
     event.preventDefault();
 
-    var inputtedPieSize = $("input.piesize").val();
+    var inputtedPieSize = $("select#pie-size").val();
     var newPizza = new Pizza(inputtedPieSize);
 
     $.each($("input[name='pietoppings']:checked"), function() {
       newPizza.pietoppings.push($(this).val());
     });
 
-    $("ul#order-list")append("<li><button type='submit' class='btn btn-primary btn-margin'>" + newPizza.piesize + "Pizza Order" + "</button></li>");
+    $("ul#order-list").append("<li><button type='submit' class='btn btn-primary btn-margin'>" + newPizza.piesize + "Pizza Order" + "</button></li>");
 
     $(".pizzaOrder").last().click(funtion() {
-      $("#order-specs").show();
+      $("#order-specifications").show();
       $(".pie-size").text(newPizza.piesize);
       $(".pie-toppings").text(newPizza.pietoppings);
       $(".order-total").text(newPizza.price());
